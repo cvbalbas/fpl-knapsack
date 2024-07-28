cdb429d8e14dc8e4d735c89c49a3c3718af2efe7
Caryl Anne Balbas <caryl.anne.balbas@eee.upd.edu.ph>
2024-07-28 14:38:39 +0100

    Heroku settings

--- app.py
+++ app.py
@@ -285,4 +285,6 @@ def get_top_players_by_position(players, top_n=40):
     
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
