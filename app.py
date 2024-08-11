c1e5bbcab8388732d68f475369869c032b5a7654
Caryl Anne Balbas <caryl.anne.balbas@eee.upd.edu.ph>
2024-08-11 15:38:03 +0100

    Add players manually

--- app.py
+++ app.py
@@ -26,60 +26,69 @@ def index():
 
 @app.route('/printTable', methods=['POST'])
 def printTable():
-    if 'file' not in request.files:
-        return jsonify({'message': 'No file part'}), 400
+    # if 'file' not in request.files:
+    #     return jsonify({'message': 'No file part'}), 400
     
-    file = request.files['file']
+    # file = request.files['file']
     
-    if file.filename == '':
-        return jsonify({'message': 'No selected file'}), 400
+    # if file.filename == '':
+    #     return jsonify({'message': 'No selected file'}), 400
     
-    if file:
-        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
-        file.save(filepath)
-        
-        # Read the CSV file into a DataFrame
-        # df = pd.read_csv(filepath)
-        players = []
-        with open(filepath, mode='r', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            # print(*reader)
-            for row in reader:
-                # print(*row)
-                if int(row['score']) > 0 and row['status'] == 'a':
-                    name = row['name']
-                    price = float(row['price'])
-                    points = float(row['score'])
-                    position = row['position']
-                    # Create a Player instance and append to the list
-                    player = Player(name=name, price=price, points=points, position=position)
-                    players.append(player)
-                    # print(player)
-        
-        # Convert the DataFrame to a JSON object
-        # csv_data = df.to_dict(orient='records')
-        # print(csv_data)
-        # Get top 50 players from each position   
-        top_players_by_position = get_top_players_by_position(players, top_n=50)
-        merged_top_players = []
-        for top_players in top_players_by_position.values():
-            merged_top_players.extend(top_players)
+    # if file:
+    # filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+    # file.save(filepath)
+    
+    # Read the CSV file into a DataFrame
+    # df = pd.read_csv(filepath)
+    filepath = "uploads/fpl_player.csv"
+    players = []
+    with open(filepath, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        # print(*reader)
+        for row in reader:
+            # print(*row)
+            name = row['name']
+            price = float(row['price'])
+            points = float(row['score'])
+            position = row['position']
+            id = int(row['id'])
+            team = row['team']
+            # Create a Player instance and append to the list
+            player = Player(name=name, price=price, points=points, position=position, id=id, team=team)
+            players.append(player)
+            # print(player)
+    
+    # Convert the DataFrame to a JSON object
+    # csv_data = df.to_dict(orient='records')
+    # print(csv_data)
+    # Get top 50 players from each position   
+    top_players_by_position = get_top_players_by_position(players, top_n=50)
+    merged_top_players = []
+    for top_players in top_players_by_position.values():
+        merged_top_players.extend(top_players)
 
-        # Sort the merged list by points in descending order
-        sorted_merged_top_players = sorted(merged_top_players, key=lambda player: player.points, reverse=True)
-        # sorted_playersDB = sorted(players, key=lambda player: player.points, reverse=True)
-        players_dict = [player.to_dict() for player in sorted_merged_top_players]
+    # Sort the merged list by points in descending order
+    sorted_merged_top_players = sorted(players, key=lambda player: player.name, reverse=False)
+    # sorted_playersDB = sorted(players, key=lambda player: player.points, reverse=True)
+    players_dict = [player.to_dict() for player in sorted_merged_top_players]
 
-        print(*players_dict)
-        return jsonify({'message': 'File successfully uploaded', 'players': players_dict}), 200
+    print(*players_dict)
+    return jsonify({'message': 'File successfully uploaded', 'players': players_dict}), 200
 
 @app.route('/run_knapsack', methods=['POST'])
 def run_knapsack():
     data = request.get_json()
     formation = int(data['formation']) 
     budget = int(data['budget'])
-    filename = data['filename'] #Save as a unique filename and that's what we pass
+    # filename = data['filename'] #Save as a unique filename and that's what we pass
+    players_data = data['selectedPlayers']
+    # print(players_data)
+    playersDB = [Player(**player) for player in players_data]
+    # for player in playersDB:
+    #     print(player)
+
     formations = [
+    # possible_formations = [
             [3, 4, 3],
             [3, 5, 2],
             [4, 3, 3],
@@ -90,33 +99,39 @@ def run_knapsack():
         ]
     possible_formations = [formations[formation]]
     
-    playersDB = create_players_from_csv(filename)
+    # playersDB = create_players_from_csv(filename)
     # print(*playersDB[:150])
     
     # Get top 50 players from each position   
-    top_players_by_position = get_top_players_by_position(playersDB, top_n=50)
-    merged_top_players = []
-    for top_players in top_players_by_position.values():
-        merged_top_players.extend(top_players)
+    # top_players_by_position = get_top_players_by_position(playersDB, top_n=10)
+    # merged_top_players = []
+    # for top_players in top_players_by_position.values():
+    #     merged_top_players.extend(top_players)
 
     # Sort the merged list by points in descending order
-    sorted_merged_top_players = sorted(merged_top_players, key=lambda player: player.points, reverse=True)
+    # sorted_merged_top_players = sorted(merged_top_players, key=lambda player: player.points, reverse=True)
     # sorted_playersDB = sorted(playersDB, key=lambda player: player.points, reverse=True)
     # print(*sorted_playersDB[:100])
 
     # print("Data successfully opened fpl_players.csv and loaded into playersDB")
-    formation =  best_full_teams(sorted_merged_top_players, possible_formations, budget)
-    best_players = formation[0][2]
-    best_players_str = ",".join(str(element) for element in best_players)
-    # print(best_players_str)
+    formation =  best_full_teams(playersDB, possible_formations, budget)
+    # print(formation)
+    # best_players = formation[0][2]
+    # best_players_str = ",".join(str(element) for element in best_players)
+    # # print(best_players_str)
 
     formation_str = ",".join(str(element) for element in formation[0][0])
-
-    data = {
-        "bestPlayers": best_players_str,
-        "formation": formation_str,
-        "score": formation[0][1]
-    }
+    data = []
+    for best_team in formation:
+        best_players = best_team[2]
+        best_players_str = ",".join(str(element) for element in best_players)
+        formation_str = ",".join(str(element) for element in best_team[0])
+        best_team_data = {
+            "bestPlayers": best_players_str,
+            "formation": formation_str,
+            "score": best_team[1]
+        }
+        data.append(best_team_data)
 
     return data
 
@@ -193,7 +208,7 @@ def generate_group(full_list, group):
     group_indexes = []
     for i, item in enumerate(full_list):
         if item.position == group:
-            group_values.append(item.points)
+            group_values.append(float(item.points))
             group_weights.append(int(item.price))
             group_indexes.append(i)
     return group_values, group_weights, group_indexes
@@ -285,6 +300,6 @@ def get_top_players_by_position(players, top_n=40):
     
     
 if __name__ == '__main__':
-    # app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
